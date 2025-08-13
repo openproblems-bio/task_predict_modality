651b2ac868b5070ce849e745daa9dc3f29893c8a
Marius Lange <marius.lange@bsse.ethz.ch>
2025-08-13 10:20:45 +0200

    Update cellmaper_scvi

--- src/methods/cellmapper_linear/script.py
+++ src/methods/cellmapper_linear/script.py
@@ -35,13 +35,12 @@ input_train_mod1.obsm["mod2"] = input_train_mod2.layers["normalized"]
 
 # choose the kNN method based on total cell number
 n_obs = input_test_mod1.n_obs + input_train_mod1.n_obs
-knn_method = "sklearn" if n_obs < 60000 else "pynndescent"
 
 print("Set up and prepare Cellmapper", flush=True)
 cmap = cm.CellMapper(query=input_test_mod1, reference=input_train_mod1)
 cmap.compute_neighbors(
-    use_rep=None, 
-    knn_method=knn_method,
+    use_rep=None,
+    knn_method="sklearn" if n_obs < 60000 else "pynndescent",
     fallback_representation=par['fallback_representation'],
     n_neighbors=par['n_neighbors'],
     fallback_kwargs={"mask_var": par['mask_var']},
