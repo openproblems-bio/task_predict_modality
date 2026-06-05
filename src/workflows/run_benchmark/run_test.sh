15a5023709c91b694d0c8512440028d9ac61a1d2
Robrecht Cannoodt <rcannood@gmail.com>
2026-06-05 12:43:57 +0200

    update script

--- src/workflows/run_benchmark/run_test.sh
+++ src/workflows/run_benchmark/run_test.sh
@@ -15,17 +15,17 @@ if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
 fi
 # run benchmark
-export NXF_VER=23.04.2
+export NXF_VER=25.10.5
 
 nextflow run . \
-  -main-script target/nextflow/predict_modality/workflows/run_benchmark/main.nf \
+  -main-script target/nextflow/workflows/run_benchmark/main.nf \
   -profile docker \
   -resume \
   -entry auto \
   -with-trace \
   -c common/nextflow_helpers/labels_ci.config \
   --input_states "$DATASETS_DIR/**/state.yaml" \
-  --rename_keys 'input_train_mod1:output_train_mod1,input_train_mod2:output_train_mod2,input_test_mod1:output_test_mod1,input_test_mod2:output_test_mod2' \
+  --rename_keys 'input_train_mod1:output_train_mod1;input_train_mod2:output_train_mod2;input_test_mod1:output_test_mod1;input_test_mod2:output_test_mod2' \
   --settings '{"output_scores": "scores.yaml", "output_dataset_info": "dataset_info.yaml", "output_method_configs": "method_configs.yaml", "output_metric_configs": "metric_configs.yaml", "output_task_info": "task_info.yaml"}' \
   --publish_dir "$OUTPUT_DIR" \
   --output_state 'state.yaml'
